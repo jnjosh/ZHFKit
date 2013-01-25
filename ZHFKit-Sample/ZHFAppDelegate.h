55e9cfe9a3ac66de57b20b04c0564f65a227c76a
Steve Foster <foster@flightblog.org>
2013-01-25 18:07:48 -0500

    First commit to ZHFKit

---
+++ ZHFKit-Sample/ZHFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ZHFAppDelegate.h
+//  ZHFKit-Sample
+//
+//  Created by apollo on 1/25/13.
+//  Copyright (c) 2013 ZHFKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ZHFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ ZHFKit-Sample/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZHFKit-Sample
+//
+//  Created by apollo on 1/25/13.
+//  Copyright (c) 2013 ZHFKit. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ZHFAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ZHFAppDelegate class]));
+    }
+}
